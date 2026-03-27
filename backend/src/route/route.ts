14b21235c6baa9213840cf32dfb9bf0d4d05b62c
R-Mohammed <Mohamed21raiss@gmail.com>
2026-03-27 19:10:10 +0100

    create angular and api git and create server

--- backend/src/route/route.ts
+++ backend/src/route/route.ts
@@ -7,16 +7,37 @@ import { Anomaly } from "../entity/anomaly";
 import { Asset } from "../entity/asset";
 import { CorrectAction } from "../entity/correctAction"; 
 import { Inspection } from "../entity/inspection";
-
-
-
-
-
-
+const port = 3004;
+const app = express();
 const router = Router();
 router.use(cors());
 router.use(express.json());
 
+
+router.get("/users", async (req, res) => {
+  const user = await AppDataSource.getRepository(users).find();
+  res.json(user);
+});
+
+router.get("/anomaly", async (req, res) => {
+  const anomaly = await AppDataSource.getRepository(Anomaly).find();
+  res.json(anomaly);
+});
+
+router.get("/asset", async (req, res) => {
+  const asset = await AppDataSource.getRepository(Asset).find();
+  res.json(asset);
+});
+
+router.get("/correctAction", async (req, res) => {
+  const correctAction = await AppDataSource.getRepository(CorrectAction).find();
+  res.json(correctAction);
+});
+
+router.get("/inspection", async (req, res) => {
+  const inspection = await AppDataSource.getRepository(Inspection).find();
+  res.json(inspection);
+});
 
 
 
@@ -25,6 +46,9 @@ router.use(express.json());
 AppDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!");
+    app.listen(port, () => {
+      console.log("Server is running on port " + port);
+    });
   })
   .catch((error) =>
     console.error("Error during Data Source initialization:", error),
