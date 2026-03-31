345e7572316118f8a22fbe6ca94eb8d1d6b2a364
R-Mohammed <Mohamed21raiss@gmail.com>
2026-03-31 18:50:59 +0100

    inspection

--- frontend/gmo/src/app/app.component.ts
+++ frontend/gmo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {GoogleMapsModule} from '@angular/google-maps';
+import { HomePageComponent } from './admin/home-page/home-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GoogleMapsModule],
+  imports: [RouterOutlet, GoogleMapsModule, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
